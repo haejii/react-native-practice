602c5300cd3e9516d03d624f984d85d69a70289d
SeaFalcon <seafalcon323@gmail.com>
2021-03-20 12:41:45 +0900

    Apply eslint & refactoring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import AuthenticationFlow from './authentication/AuthenticationFlow';
-import KakaoLogin from './authentication/KakaoLogin';
 import store from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      {/* <KakaoLogin /> */}
       <AuthenticationFlow />
     </Provider>
   );
