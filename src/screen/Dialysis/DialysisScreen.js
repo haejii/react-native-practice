c7904c336cf196592fa7b2cf143a256186e68d19
Haeji <haillydev@gmail.com>
2021-04-30 19:44:24 +0900

    Refactoring DialysisScreen

--- src/screen/Dialysis/DialysisScreen.js
+++ src/screen/Dialysis/DialysisScreen.js
@@ -1,12 +1,10 @@
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import React from 'react';
 
-import MachineDialysis from './MachinedialysisScreen';
-import GeneralDialysis from './GeneralDialysisScreen';
-import AgendaScreen from './Calende';
 import Hemodialysis from './Hemodialysis/Hemodialysis';
 import {useSelector} from 'react-redux';
 import kidneyTypes from '../../utils/kidneyType';
+import Peritonealdialysis from './Peritonealdialysis/PeritonealdialysisMain';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -14,25 +12,7 @@ export default function DialysisScreen() {
   const kidneyType = useSelector((state) => state.user.kidneyType);
 
   if (kidneyTypes[kidneyType] === '복막투석') {
-    return (
-      <Tab.Navigator initialRouteName="Canlendar">
-        <Tab.Screen
-          name="Canlendar"
-          component={AgendaScreen}
-          options={{tabBarLabel: '달력'}}
-        />
-        <Tab.Screen
-          name="GenetalDialysis"
-          component={GeneralDialysis}
-          options={{tabBarLabel: '일반복막투석'}}
-        />
-        <Tab.Screen
-          name="MachineDialysis"
-          component={MachineDialysis}
-          options={{tabBarLabel: '기계복막투석'}}
-        />
-      </Tab.Navigator>
-    );
+    return <Peritonealdialysis />;
   }
 
   return <Hemodialysis />;
