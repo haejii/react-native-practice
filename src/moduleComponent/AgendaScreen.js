d1e81c2b875cf50e18919f42b52fcdbe1b2fb2bf
hailly01 <yanghagi@naver.com>
2021-07-01 08:26:18 +0900

    Add TopBar in DialysisScreen

--- src/moduleComponent/AgendaScreen.js
+++ src/moduleComponent/AgendaScreen.js
@@ -12,6 +12,7 @@ import {Agenda} from 'react-native-calendars';
 
 import {useSelector, useDispatch} from 'react-redux';
 import {fetchMemos} from '../actions';
+import {BarStyle} from '../style/styles';
 import {getFormattedDate} from '../utils/functions';
 
 function AgendaScreen({navigation}) {
@@ -93,32 +94,35 @@ function AgendaScreen({navigation}) {
   };
 
   return (
-    <Agenda
-      theme={{
-        agendaDayNumColor: 'green',
-        agendaTodayColor: 'red',
-        agendaKnobColor: 'blue',
-      }}
-      items={dialysisMemos}
-      // loadItemsForMonth={({dateString}) => loadItems(dateString)}
-      onDayPress={({dateString}) => loadItems(dateString)}
-      selected={new Date()}
-      renderItem={(item) => renderItem(item)}
-      renderEmptyDate={(emptyDate) => renderEmptyDate(emptyDate)}
-      rowHasChanged={(r1, r2) => rowHasChanged(r1, r2)}
+    <>
+      <View style={BarStyle.ViewContainer} />
+      <Agenda
+        theme={{
+          agendaDayNumColor: 'green',
+          agendaTodayColor: 'red',
+          agendaKnobColor: 'blue',
+        }}
+        items={dialysisMemos}
+        // loadItemsForMonth={({dateString}) => loadItems(dateString)}
+        onDayPress={({dateString}) => loadItems(dateString)}
+        selected={new Date()}
+        renderItem={(item) => renderItem(item)}
+        renderEmptyDate={(emptyDate) => renderEmptyDate(emptyDate)}
+        rowHasChanged={(r1, r2) => rowHasChanged(r1, r2)}
 
-      // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly.
-      // onRefresh={() => {
-      //   console.log('refreshing...');
-      //   dispatch(fetchMemos(new Date()));
-      // }}
+        // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly.
+        // onRefresh={() => {
+        //   console.log('refreshing...');
+        //   dispatch(fetchMemos(new Date()));
+        // }}
 
-      // Set this true while waiting for new data from a refresh
-      // refreshing={true}
+        // Set this true while waiting for new data from a refresh
+        // refreshing={true}
 
-      // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView.
-      // refreshControl={null}
-    />
+        // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView.
+        // refreshControl={null}
+      />
+    </>
   );
 }
 
