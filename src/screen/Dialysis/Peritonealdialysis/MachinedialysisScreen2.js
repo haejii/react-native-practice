b139e0377412bd941ce11bd0f0e1bde76eac58e2
haejii <yanghagi@naver.com>
2021-07-08 23:19:55 +0900

    Refactoring Write PertionealDialysis

--- src/screen/Dialysis/Peritonealdialysis/MachinedialysisScreen2.js
+++ src/screen/Dialysis/Peritonealdialysis/MachinedialysisScreen2.js
@@ -27,9 +27,7 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 import SplashScreen from '../../SplashScreen';
 import errors from '../../../utils/errors';
 
-export default function Machinedialysis2({
-  navigation
-}) {
+export default function Machinedialysis2({navigation}) {
   const dispatch = useDispatch();
 
   const kidneyType = useSelector((state) => state.user.kidneyType);
@@ -38,168 +36,383 @@ export default function Machinedialysis2({
   const [show, setShow] = useState(false);
   const [photo, setPhoto] = useState(null);
   const [exchangeTime, setExchangeTime] = useState(new Date());
+  const [month, setMonth] = useState(exchangeTime.getMonth() + 1);
+  const [today, setToday] = useState(exchangeTime.getDate());
   const [hour, setHour] = useState(exchangeTime.getHours());
   const [min, setMin] = useState(exchangeTime.getMinutes());
 
   const [btn, setBtn] = useState(1);
 
   const dialysis = useSelector((state) => state.dialysis);
-  const dialysisType = 1;
+  let time = `${hour}시 ${min}분`;
+  let day = `${month + 1}월 ${today}일`;
 
+  // Change TextInput
   function handleChangDialysis(name, value) {
     dispatch(changeDialysis(name, value));
   }
 
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || exchangeTime;
+    setShow(Platform.OS === 'ios');
+    setExchangeTime(currentDate);
+    setHour(currentDate.getHours());
+    setMin(currentDate.getMinutes());
+    setMonth(currentDate.getMonth());
+    setToday(currentDate.getDate());
 
+    const formattedSelectedDate = `${currentDate.getFullYear()}-${
+      currentDate.getMonth() + 1
+    }-${currentDate.getDate()}`;
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+
+  const handlePressShowImagePicker = () => {
+    Alert.alert('선택해주세요', '', [
+      {
+        text: '사진 찍기',
+        onPress: () => Alert.alert('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {
+        text: '갤러리에서 불러오기',
+        onPress: () => {
+          launchImageLibrary({}, (response) => {
+            console.log('Response = ', response);
+
+            if (response.didCancel) {
+              console.log('User cancelled image picker');
+            } else if (response.error) {
+              console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+              console.log('User tapped custom button: ', response.customButton);
+              Alert.alert(response.customButton);
+            } else {
+              setPhoto(response);
+            }
+          });
+        },
+        style: 'cancel',
+      },
+      {
+        text: '닫기',
+        style: 'cancel',
+      },
+    ]);
+  };
+  // a:question
   const a = [
-  
-    {"content": String(dialysis.injectionAmount), "explain" : "주입량(g)을 숫자로 입력해주세요", "name": "injectionAmount"},
-    {"content": String(dialysis.drainage), "explain" : "배액량(g)을 숫자로 입력해주세요", "name":"drainage" },
-    {"content": String(dialysis.dehydration), "explain" : "제수량(g)을 숫자로 입력해주세요", "name" :"dehydration" },
-    {"content": String(dialysis.weight), "explain" : "몸무개를 측정하셨나요?", "name" :"weight"}
+    {
+      content: String(dialysis.injectionAmount),
+      explain: '주입량(g)을 숫자로 입력해주세요',
+      name: 'injectionAmount',
+    },
+    {
+      content: String(dialysis.drainage),
+      explain: '배액량(g)을 숫자로 입력해주세요',
+      name: 'drainage',
+    },
+    {
+      content: String(dialysis.dehydration),
+      explain: '제수량(g)을 숫자로 입력해주세요',
+      name: 'dehydration',
+    },
+    {
+      content: String(dialysis.weight),
+      explain: '몸무게를 측정하셨나요?',
+      name: 'weight',
+    },
   ];
-  return(
+
+  // b:question
+  const b = [
+    {
+      content: String(dialysis.injectionAmount),
+      explain: '주입량(g)을 숫자로 입력해주세요',
+      name: 'injectionAmount',
+    },
+    {
+      content: String(dialysis.drainage),
+      explain: '배액량(g)을 숫자로 입력해주세요',
+      name: 'drainage',
+    },
+    {
+      content: String(dialysis.dehydration),
+      explain: '제수량(g)을 숫자로 입력해주세요',
+      name: 'dehydration',
+    },
+    {
+      content: String(dialysis.weight),
+      explain: '몸무게를 측정하셨나요?',
+      name: 'weight',
+    },
+    {
+      content: String(dialysis.bloodPressure),
+      explain: '혈압을 측정하셨나요?',
+      name: 'weight',
+    },
+    {
+      content: String(dialysis.bloodSugar),
+      explain: '혈당을 측정하셨나요?',
+      name: 'weight',
+    },
+  ];
+
+  return (
     <View style={{backgroundColor: 'white'}}>
-      
-      <View style={{marginLeft: 10, marginTop: 10}}>
-        <Text>Calendar</Text>
-      </View>
-    
       <ScrollView>
-
-      <View style={{height: 150, backgroundColor: 'skyblue', margin: 15}}>
-        <Text>그래프</Text>
-      </View>
-
-      {/* <View style={{height: 50, marginTop: 10, marginLeft: 15}}>
-        <Text style={{ fontFamily: 'NotoSansKR-Medium', fontSize: 17}}>기계복막투석</Text>
-      </View>
-       */}
-      <View style={{marginLeft: 10, marginTop: 10, alignContent: 'center', justifyContent: 'center' }}>
-        {/* <View style={{marginBottom: 20}}> */}
+        <View style={{margin: 20, flex: 1}}>
+          <Text style={{fontSize: 20}}>투석일지 작성</Text>
+        </View>
+        <View
+          style={{
+            marginLeft: 10,
+            marginTop: 10,
+            alignContent: 'center',
+            justifyContent: 'center',
+            flex: 6,
+          }}>
+          <View>
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <Text>복막투석을 선택해주세요. </Text>
+            </View>
+            <View
+              style={{
+                alignItems: 'center',
+                width: '100%',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <NativeButton
+                style={{
+                  width: 150,
+                  marginRight: 15,
+                  backgroundColor: btn === 1 ? 'skyblue' : 'white',
+                }}
+                onPress={() => setBtn(1)}>
+                일반 복막투석
+              </NativeButton>
+              <NativeButton
+                style={{
+                  width: 150,
+                  backgroundColor: btn === 2 ? 'skyblue' : 'white',
+                }}
+                onPress={() => setBtn(2)}>
+                기계 복막 투석
+              </NativeButton>
+            </View>
+          </View>
 
           <View>
-            <Text>복막투석 방법을 선택하세요. </Text>
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+              <Text>투석일지 작성 </Text>
+            </View>
             <View
+              style={{
+                alignItems: 'center',
+                width: '100%',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <NativeButton
                 style={{
-                  alignItems: 'center',
-                  width: '100%',
-                  flexDirection: 'row',
-                  alignItems: 'center', justifyContent: 'center'
-                }}>
-          
-                <NativeButton style={{width: 150, marginRight: 15, backgroundColor: btn === 1 ? 'skyblue': 'white'}} onPress={()=>setBtn(1)}>일반 복막투석</NativeButton>
-                <NativeButton style={{width: 150, backgroundColor: btn === 2 ? 'skyblue': 'white'}} onPress={()=>setBtn(2)}>기계 복막 투석</NativeButton>
-              </View>
-           </View>
-           {btn === 1? (<Text>1번이당!</Text>) : (
-             <>
-              <View style={{marginBottom: 15, alignContent: 'center', justifyContent: 'center', alignItems: 'center', justifyContent: 'center'}}>
-           <Text style={{fontFamily: 'NotoSansKR-Medium', fontSize: 15}}> 오늘이 몇 번째인지 선택하세요(7월 첫째주)</Text>
-         </View>
-             <View
-             style={{
-               alignItems: 'center',
-               width: '100%',
-               flexDirection: 'row',
-               alignItems: 'center', justifyContent: 'center'
-             }}>
-       
-             <NativeButton style={{width: 150, marginRight: 15}}>1차</NativeButton>
-             <NativeButton style={{width: 150}}>2차</NativeButton>
-           </View>
-           <View
+                  width: 150,
+                  marginRight: 15,
+                }}
+                onPress={showDatepicker}>
+                {day}
+              </NativeButton>
+              <NativeButton
                 style={{
-                  alignItems: 'center',
-                  width: '100%',
-                  flexDirection: 'row',
-                  alignItems: 'center', justifyContent: 'center'
-                }}>
-          
-                <NativeButton style={{width: 150, marginRight: 15}}>3차</NativeButton>
-                <NativeButton style={{width: 150}}>4차</NativeButton>
-              </View>
-
-              
-        {Object.keys(a).map((key, i) => {
-          return(
-            <View style={DialysisScreenStyle.basicView}>
-            <Text style={{fontFamily: 'NotoSansKR-Medium', fontSize: 15}}>{a[key]["explain"]}</Text>
-            <View style={{flexDirection: 'row'}}>
-            <TextInput
-              style={DialysisScreenStyle.basicTextInput}
-              keyboardType="numeric"
-              value={a[key]["content"]}
-              onChangeText={(value) =>
-                handleChangDialysis(a[key]["name"], value)
-              }
-            />
-       
+                  width: 150,
+                }}
+                onPress={showTimepicker}>
+                {time}
+              </NativeButton>
             </View>
+            {show && (
+              <DateTimePicker
+                testID="dateTimePicker"
+                value={exchangeTime}
+                mode={mode}
+                is24Hour={true}
+                display="default"
+                onChange={onChange}
+              />
+            )}
           </View>
-           
-           
-          )
-        })}
-              
-            
 
-           </>
-           )
-}
-         
-          {/* <View style={{marginBottom: 15, alignContent: 'center', justifyContent: 'center', alignItems: 'center', justifyContent: 'center'}}>
-            <Text style={{fontFamily: 'NotoSansKR-Medium', fontSize: 15}}> 오늘이 몇 번째인지 선택하세요(7월 첫째주)</Text>
-          </View> */}
-    
-                {/* <View
+          {btn === 1 ? (
+            <>
+              <View
+                style={{
+                  marginBottom: 15,
+                  alignContent: 'center',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Text style={{fontFamily: 'NotoSansKR-Medium', fontSize: 15}}>
+                  {' '}
+                  오늘이 몇 번째인지 선택하세요(7월 첫째주)
+                </Text>
+              </View>
+              <View
                 style={{
                   alignItems: 'center',
                   width: '100%',
                   flexDirection: 'row',
-                  alignItems: 'center', justifyContent: 'center'
+                  alignItems: 'center',
+                  justifyContent: 'center',
                 }}>
-          
-                <NativeButton style={{width: 150, marginRight: 15}}>1차</NativeButton>
+                <NativeButton style={{width: 150, marginRight: 15}}>
+                  1차
+                </NativeButton>
                 <NativeButton style={{width: 150}}>2차</NativeButton>
-              </View> */}
-
-
-              {/* <View
+              </View>
+              <View
                 style={{
                   alignItems: 'center',
                   width: '100%',
                   flexDirection: 'row',
-                  alignItems: 'center', justifyContent: 'center'
+                  alignItems: 'center',
+                  justifyContent: 'center',
                 }}>
-          
-                <NativeButton style={{width: 150, marginRight: 15}}>3차</NativeButton>
+                <NativeButton style={{width: 150, marginRight: 15}}>
+                  3차
+                </NativeButton>
                 <NativeButton style={{width: 150}}>4차</NativeButton>
-              </View>*/}
-              {/* </View>  */}
-{/* 
-        {Object.keys(a).map((key, i) => {
-          return(
+              </View>
+              {Object.keys(b).map((key, i) => {
+                return (
+                  <View key={i + 1} style={DialysisScreenStyle.basicView}>
+                    <Text
+                      style={{fontFamily: 'NotoSansKR-Medium', fontSize: 15}}>
+                      {b[key]['explain']}
+                    </Text>
+                    <View style={{flexDirection: 'row'}}>
+                      <TextInput
+                        style={DialysisScreenStyle.basicTextInput}
+                        keyboardType="numeric"
+                        value={b[key]['content']}
+                        onChangeText={(value) =>
+                          handleChangDialysis(b[key]['name'], value)
+                        }
+                      />
+                    </View>
+                  </View>
+                );
+              })}
+            </>
+          ) : (
+            <>
+              {Object.keys(a).map((key, i) => {
+                return (
+                  <View key={i + 1} style={DialysisScreenStyle.basicView}>
+                    <Text
+                      style={{fontFamily: 'NotoSansKR-Medium', fontSize: 15}}>
+                      {a[key]['explain']}
+                    </Text>
+                    <View style={{flexDirection: 'row'}}>
+                      <TextInput
+                        style={DialysisScreenStyle.basicTextInput}
+                        keyboardType="numeric"
+                        value={a[key]['content']}
+                        onChangeText={(value) =>
+                          handleChangDialysis(a[key]['name'], value)
+                        }
+                      />
+                    </View>
+                  </View>
+                );
+              })}
+            </>
+          )}
+
+          <View style={DialysisScreenStyle.basicView}>
+            <Text>첨부할 사진이 있나요?</Text>
+            <TouchableOpacity
+              style={{
+                width: '80%',
+                borderRadius: 20,
+                height: 300,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'skyblue',
+              }}
+              onPress={() => handlePressShowImagePicker()}>
+              {photo ? (
+                <Image
+                  source={{
+                    uri: photo && photo.uri ? photo.uri : photo,
+                  }}
+                  style={{
+                    resizeMode: 'stretch',
+                    borderRadius: 20,
+                    width: '100%',
+                    height: 300,
+                  }}
+                />
+              ) : (
+                <Text
+                  style={{
+                    fontSize: 24,
+                    fontWeight: 'bold',
+                    color: 'white',
+                  }}>
+                  사진 첨부하기
+                </Text>
+              )}
+            </TouchableOpacity>
+          </View>
+
+          <View style={{flex: 3, marginTop: 20}}>
+            <Text>메모 </Text>
             <View style={DialysisScreenStyle.basicView}>
-            <Text style={{fontFamily: 'NotoSansKR-Medium', fontSize: 15}}>{a[key]["explain"]}</Text>
-            <View style={{flexDirection: 'row'}}>
-            <TextInput
-              style={DialysisScreenStyle.basicTextInput}
-              keyboardType="numeric"
-              value={a[key]["content"]}
-              onChangeText={(value) =>
-                handleChangDialysis(a[key]["name"], value)
-              }
-            />
-       
+              <TextInput
+                style={{
+                  width: '95%',
+                  backgroundColor: 'white',
+                  height: 100,
+                  paddingHorizontal: 10,
+                  fontSize: 16,
+                  marginBottom: '1%',
+                  marginRight: '5%',
+                  borderWidth: 1,
+                  borderColor: 'black',
+                }}
+                value={String(dialysis.memo)}
+                onChangeText={(value) => handleChangDialysis('memo', value)}
+              />
             </View>
           </View>
-           
-           
-          )
-        })} */}
-      </View>
+
+          <NativeButton
+            style={{
+              margin: 10,
+            }}
+            onPress={() => {
+              // dispatch(clearDialysis());
+              // AddBtn();
+            }}>
+            저장하기
+          </NativeButton>
+        </View>
       </ScrollView>
     </View>
-  )
+  );
 }
